832fc2e9cbe33fbef50e81b77240c8ab9fd15ccc
iwenking <201468291@qq.com>
2020-05-24 19:43:53 +0800

    新建路由菜单

---
+++ src/pages/charts/line.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class line extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default line;
